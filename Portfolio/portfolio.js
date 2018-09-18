e49d90240cf5ed15bb60b29509d8ef4d18b7b752
robdelm33 <r.d.mcfarlin@gmail.com>
2018-09-18 17:29:06 +0000

    Added JS/CSS background change

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,11 @@
+function navbarBlue() { 
+    document.querySelector(".querySetter").style.background = "blue";
+}
+
+function navbarRed() { 
+    document.querySelector(".querySetter").style.background = "red";
+}
+
+function navbarGreen() { 
+    document.querySelector(".querySetter").style.background = "green";
+}
