ca44f16cd0a4072644806709a9d4b76ba365790c
robdelm33 <r.d.mcfarlin@gmail.com>
2018-09-24 01:35:04 +0000

    Styling and seciton changes

--- js/script.js
+++ js/script.js
@@ -13,5 +13,41 @@ $(document).ready(function() {
 		startDelay: 1000,
 		showCursor: false
 	});
+	
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		items: 4,
+		responsive:{
+			0:{
+				items:1
+			},
+			480:{
+				items: 2	
+			},
+			768:{
+				items:3
+			},
+			938:{
+				items:4
+			}
+		}
+	});
+    
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    $(window).scroll(function() {
+    	if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+    		$('.chart').easyPieChart({
+		        easing: "easeInOut",
+		        barColor: "#dfe6e9",
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 6,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });	
+    	}
+    });
 
 });
