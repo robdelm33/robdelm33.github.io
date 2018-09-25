605312d4d95ddb2f5912f39b45c46b8650f87c65
robdelm33 <r.d.mcfarlin@gmail.com>
2018-09-25 18:32:14 +0000

    Added certifications

--- js/script.js
+++ js/script.js
@@ -106,5 +106,24 @@ $(document).ready(function() {
 			queue: false
 		}
 	});
+	
+	$("#filters a").click(function() {
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
+		
+		var selector = $(this).attr("data-filter");
+		
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
+		
+		return false;
+		
+	});
 
 });
